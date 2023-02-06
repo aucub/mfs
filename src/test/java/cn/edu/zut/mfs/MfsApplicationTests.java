e826a6bb06a28eb21bfd1fbbe60ab0bda797deff
aucub <78630225+aucub@users.noreply.github.com>
2023-02-06 08:07:05 +0800

    Initial commit

---
+++ src/test/java/cn/edu/zut/mfs/MfsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zut.mfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
