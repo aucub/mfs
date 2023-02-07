c710d0015df9f13ae771c01334ad0dcdf3d0c3c4
aucub <78630225+aucub@users.noreply.github.com>
2023-02-07 22:42:58 +0800

    添加配置文件

--- src/main/java/cn/edu/zut/mfs/controller/UserController.java
+++ src/main/java/cn/edu/zut/mfs/controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.edu.zut.mfs.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
+import cn.edu.zut.mfs.config.BaseResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,13 @@ public class UserController {
 
     // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
     @RequestMapping("doLogin")
-    public String doLogin(String username, String password) {
+    public BaseResponse<String> doLogin(String username, String password) {
         // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
         if("zhang".equals(username) && "123456".equals(password)) {
             StpUtil.login(10001);
-            return "登录成功";
+            return BaseResponse.success("登录成功");
         }
-        return "登录失败";
+        return BaseResponse.fail("9999","登录失败");
     }
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
