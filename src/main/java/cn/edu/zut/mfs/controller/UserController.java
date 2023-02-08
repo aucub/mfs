a5a24fb588ec27c80327ff3a00003a661c2e5022
aucub <78630225+aucub@users.noreply.github.com>
2023-02-08 08:05:35 +0800

    引入一些用户登录，权限管理接口

--- src/main/java/cn/edu/zut/mfs/controller/UserController.java
+++ src/main/java/cn/edu/zut/mfs/controller/UserController.java
@@ -29,15 +29,23 @@ public class UserController {
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
     @RequestMapping("isLogin")
-    public String isLogin() {
-        return "当前会话是否登录：" + StpUtil.isLogin();
+    public BaseResponse<String> isLogin() {
+        // StpUtil.isLogin() 查询当前客户端是否登录，返回 true 或 false
+        boolean isLogin = StpUtil.isLogin();
+        return BaseResponse.success("当前会话是否登录：" + isLogin);
     }
 
     // 注销登录，浏览器访问： http://localhost:8081/user/logout
     @RequestMapping("logout")
     public BaseResponse<String> logout() {
-        // 当前会话注销登录
+        // 退出登录会清除三个地方的数据：
+        // 		1、Redis中保存的 Token 信息
+        // 		2、当前请求上下文中保存的 Token 信息
+        // 		3、Cookie 中保存的 Token 信息（如果未使用Cookie模式则不会清除）
         StpUtil.logout();
+        // StpUtil.logout() 在未登录时也是可以调用成功的，
+        // 也就是说，无论客户端有没有登录，执行完 StpUtil.logout() 后，都会处于未登录状态
+        log.info("当前是否处于登录状态：" + StpUtil.isLogin());
         return BaseResponse.success("注销登录成功");
     }
 
@@ -46,8 +54,16 @@ public class UserController {
     public BaseResponse<String> checkLogin() {
         // 检验当前会话是否已经登录, 如果未登录，则抛出异常：`NotLoginException`
         StpUtil.checkLogin();
+        // 抛出异常后，代码将走入全局异常处理（GlobalException.java），如果没有抛出异常，则代表通过了登录校验，返回下面信息
         return BaseResponse.success("当前已登录");
     }
 
+    // 获取当前会话账号id，浏览器访问： http://localhost:8081/user/getLoginIdAsString
+    @RequestMapping("getLoginIdAsString")
+    public BaseResponse<String> getLoginIdAsString() {
+        // 获取当前会话账号id, 如果未登录，则抛出异常：`NotLoginException`
+        String userId = StpUtil.getLoginIdAsString();
+        return BaseResponse.success("当前客户端登录的账号id是：" + userId);
+    }
 
 }
---
+++ src/main/java/cn/edu/zut/mfs/handler/GlobalExceptionHandler.java
@@ -0,0 +1,104 @@
+package cn.edu.zut.mfs.handler;
+
+import cn.dev33.satoken.exception.*;
+import cn.dev33.satoken.util.SaResult;
+import cn.edu.zut.mfs.config.ApiResponse;
+import cn.edu.zut.mfs.exception.JsonException;
+import cn.edu.zut.mfs.exception.PageException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 统一异常处理
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    private static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 统一 json 异常处理
+     *
+     * @param exception JsonException
+     * @return 统一返回 json 格式
+     */
+    @ExceptionHandler(value = JsonException.class)
+    @ResponseBody
+    public ApiResponse jsonErrorHandler(JsonException exception) {
+        log.error("【JsonException】:{}", exception.getMessage());
+        return ApiResponse.ofException(exception);
+    }
+
+    /**
+     * 统一 页面 异常处理
+     *
+     * @param exception PageException
+     * @return 统一跳转到异常页面
+     */
+    @ExceptionHandler(value = PageException.class)
+    public ModelAndView pageErrorHandler(PageException exception) {
+        log.error("【DemoPageException】:{}", exception.getMessage());
+        ModelAndView view = new ModelAndView();
+        view.addObject("message", exception.getMessage());
+        view.setViewName(DEFAULT_ERROR_VIEW);
+        return view;
+    }
+
+    // 拦截：未登录异常
+    @ExceptionHandler(NotLoginException.class)
+    public SaResult handlerException(NotLoginException e) {
+
+        // 打印堆栈，以供调试
+        e.printStackTrace();
+
+        // 返回给前端
+        return SaResult.error(e.getMessage());
+    }
+
+    // 拦截：缺少权限异常
+    @ExceptionHandler(NotPermissionException.class)
+    public SaResult handlerException(NotPermissionException e) {
+        e.printStackTrace();
+        return SaResult.error("缺少权限：" + e.getPermission());
+    }
+
+    // 拦截：缺少角色异常
+    @ExceptionHandler(NotRoleException.class)
+    public SaResult handlerException(NotRoleException e) {
+        e.printStackTrace();
+        return SaResult.error("缺少角色：" + e.getRole());
+    }
+
+    // 拦截：二级认证校验失败异常
+    @ExceptionHandler(NotSafeException.class)
+    public SaResult handlerException(NotSafeException e) {
+        e.printStackTrace();
+        return SaResult.error("二级认证校验失败：" + e.getService());
+    }
+
+    // 拦截：服务封禁异常
+    @ExceptionHandler(DisableServiceException.class)
+    public SaResult handlerException(DisableServiceException e) {
+        e.printStackTrace();
+        return SaResult.error("当前账号 " + e.getService() + " 服务已被封禁 (level=" + e.getLevel() + ")：" + e.getDisableTime() + "秒后解封");
+    }
+
+    // 拦截：Http Basic 校验失败异常
+    @ExceptionHandler(NotBasicAuthException.class)
+    public SaResult handlerException(NotBasicAuthException e) {
+        e.printStackTrace();
+        return SaResult.error(e.getMessage());
+    }
+
+    // 拦截：其它所有异常
+    @ExceptionHandler(Exception.class)
+    public SaResult handlerException(Exception e) {
+        e.printStackTrace();
+        return SaResult.error(e.getMessage());
+    }
+
+}
+
