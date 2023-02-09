dd44a2b26723299c7e45bd0075eec86443d346ed
aucub <78630225+aucub@users.noreply.github.com>
2023-02-09 14:32:19 +0800

    更新一些配置文件

---
+++ src/main/java/cn/edu/zut/mfs/handler/CommonFieldHandler.java
@@ -0,0 +1,29 @@
+package cn.edu.zut.mfs.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 通用字段填充
+ */
+@Slf4j
+@Component
+public class CommonFieldHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("lastUpdateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.setFieldValByName("lastUpdateTime", new Date(), metaObject);
+    }
+}
