6f3fabf55a671223485948de7109f7560d10377f
aucub <78630225+aucub@users.noreply.github.com>
2023-02-13 08:02:14 +0800

    更新一些登录接口

--- src/main/java/cn/edu/zut/mfs/controller/SearchSessionController.java
+++ src/main/java/cn/edu/zut/mfs/controller/SearchSessionController.java
@@ -3,6 +3,8 @@ package cn.edu.zut.mfs.controller;
 import cn.dev33.satoken.session.SaSession;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.edu.zut.mfs.pojo.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,33 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Sa-Token 会话查询示例
+ * Sa-Token 会话查询
  */
+@Tag(name = "会话查询")
 @RestController
 @RequestMapping("/search/")
 public class SearchSessionController {
 
-	/*
-	 * 测试步骤：
-	 	1、先登录
-
-	 	2、根据分页参数获取会话列表
-	 		http://localhost:8081/search/getList?start=0&size=10
-	 */
-
-    // 会话查询接口  ---- http://localhost:8081/search/getList?start=0&size=10
+    @Operation(summary = "会话查询接口----根据分页参数获取会话列表")
     @RequestMapping("getList")
     public BaseResponse<List> getList(int start, int size) {
         // 创建集合
         List<SaSession> sessionList = new ArrayList<>();
-
         // 分页查询数据
         List<String> sessionIdList = StpUtil.searchSessionId("", start, size, false);
         for (String sessionId : sessionIdList) {
             SaSession session = StpUtil.getSessionBySessionId(sessionId);
             sessionList.add(session);
         }
-
         // 返回
         return BaseResponse.success(sessionList);
     }
