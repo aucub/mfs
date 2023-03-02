098e64c2c15010ab6d548c6c2ee950efa260af5e
aucub <78630225+aucub@users.noreply.github.com>
2023-03-03 01:51:54 +0800

    分离测试

--- src/main/java/cn/edu/zut/mfs/controller/RSocketController.java
+++ src/main/java/cn/edu/zut/mfs/controller/RSocketController.java
@@ -3,10 +3,15 @@ package cn.edu.zut.mfs.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * RSocket
@@ -17,6 +22,33 @@ import java.time.Duration;
 @MessageMapping("message")
 public class RSocketController {
 
+    private final List<RSocketRequester> CLIENTS = new ArrayList<>();
+
+
+    @ConnectMapping("client")
+    void connectShellClientAndAskForTelemetry(RSocketRequester requester,
+                                              @Payload String client) {
+
+        requester.rsocket()
+                .onClose()
+                .doFirst(() -> {
+                    // Add all new clients to a client list
+                    log.info("Client: {} CONNECTED.", client);
+                    CLIENTS.add(requester);
+                })
+                .doOnError(error -> {
+                    // Warn when channels are closed by clients
+                    log.warn("Channel to client {} CLOSED", client);
+                })
+                .doFinally(consumer -> {
+                    // Remove disconnected clients from the client list
+                    CLIENTS.remove(requester);
+                    log.info("Client {} DISCONNECTED", client);
+                })
+                .subscribe();
+    }
+
+
     /**
      * 当收到一个新的请求命令时，一个新的事件流被启动并返回给客户端。
      */
@@ -25,19 +57,20 @@ public class RSocketController {
         log.info("收到流请求");
         return Flux
                 // 创建一个新的索引 Flux 每秒发射一个元素
-                .interval(Duration.ofSeconds(3))
+                .interval(Duration.ofSeconds(1))
                 // 使用索引的 Flux 创建新消息的 Flux
-                .map(index -> "test").log();
+                .map(index -> new String("test" + index));
+
     }
 
     @MessageMapping("channel")
-    Flux<String> channel(final Flux<Duration> settings) {
-        log.info("Received channel request...");
-        return settings
-                .doOnNext(setting -> log.info("Channel frequency setting is {} second(s).", setting.getSeconds()))
-                .doOnCancel(() -> log.warn("The client cancelled the channel."))
-                .switchMap(setting -> Flux.interval(setting)
-                        .map(index -> ""));
+    Flux<String> channel(final Flux<String> msg) {
+        return msg
+                .doOnNext(in -> log.info("收到消息：  {}", in))
+                .map(in -> {
+                    return new String("发送的消息：" + in);
+                });
     }
+
 }
 
