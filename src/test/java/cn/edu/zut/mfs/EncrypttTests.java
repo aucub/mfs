196d3ce532f8894a6646ca0475a19f5b6fd4f564
aucub <78630225+aucub@users.noreply.github.com>
2023-03-11 23:27:08 +0800

    update test

--- src/test/java/cn/edu/zut/mfs/EncrypttTests.java
+++ src/test/java/cn/edu/zut/mfs/EncrypttTests.java
@@ -3,22 +3,48 @@ package cn.edu.zut.mfs;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.asymmetric.ECIES;
 import cn.hutool.crypto.asymmetric.KeyType;
+import com.google.crypto.tink.*;
+import com.google.crypto.tink.aead.AeadConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @SpringBootTest
 public class EncrypttTests {
     final ECIES ecies = new ECIES();
     String text = "我是一段特别长的测试";
 
+    Aead kekAead = null;
+
     @Test
     public void Test() {
         System.out.println(ecies.getPrivateKeyBase64());
         System.out.println(ecies.getPublicKeyBase64());
-        String encryptStr = ecies.encryptBase64(text.toString(), KeyType.PublicKey);
+        String encryptStr = ecies.encryptBase64(text, KeyType.PublicKey);
         String decryptStr = StrUtil.utf8Str(ecies.decrypt(encryptStr, KeyType.PrivateKey));
         System.out.println(encryptStr);
         System.out.println(decryptStr);
+    }
+
+    @Test
+    public void test1() {
+        try {
+            AeadConfig.register();
+            KeysetHandle handle = KeysetHandle.generateNew(KeyTemplates.get("AES128_GCM"));
+            kekAead = handle.getPrimitive(Aead.class);
+            handle.write(JsonKeysetWriter.withOutputStream(new FileOutputStream("./src/main/resources/keyset.json")), kekAead);
+            handle = KeysetHandle.read(JsonKeysetReader.withInputStream(new FileInputStream("./src/main/resources/keyset.json")), kekAead);
+            kekAead = handle.getPrimitive(Aead.class);
+            byte[] ciphertext = kekAead.encrypt("test".getBytes(), "one".getBytes());
+            byte[] plaintext = kekAead.decrypt(ciphertext, "one".getBytes());
+            System.out.println(new String(plaintext));
+        } catch (GeneralSecurityException | IOException ex) {
+            System.out.println("test0");
+        }
     }
 
 }
