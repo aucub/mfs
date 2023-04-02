fbcaff42c3f55ae3256550b8c22e4b82382d206a
aucub <78630225+aucub@users.noreply.github.com>
2023-04-02 18:12:36 +0800

    fix(Role): Constructor
    Signed-off-by: aucub <78630225+aucub@users.noreply.github.com>

--- src/main/java/cn/edu/zut/mfs/domain/Role.java
+++ src/main/java/cn/edu/zut/mfs/domain/Role.java
@@ -3,10 +3,12 @@ package cn.edu.zut.mfs.domain;
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @TableName("role")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @TableId(value = "id", type = IdType.AUTO)
     private String id;
