e59f607582b0845b9cad34d2d1f3aaba531e406f
aucub <78630225+aucub@users.noreply.github.com>
2023-04-12 01:31:27 +0800

    fix(jwt): generation jwt

---
+++ src/main/java/cn/edu/zut/mfs/dto/JwtDto.java
@@ -0,0 +1,21 @@
+package cn.edu.zut.mfs.dto;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtDto {
+    private String jwtId;
+    private String issuer;//签发者
+    private String subject;//主题
+    private Date expiresAt;//过期时间,Date.from(Instant.now().plus(30, ChronoUnit.MINUTES))
+    private String audience;//接收人
+    private String scope;//授权范围
+
+}
---
+++ src/main/java/cn/edu/zut/mfs/utils/JwtUtils.java
@@ -0,0 +1,22 @@
+package cn.edu.zut.mfs.utils;
+
+import cn.edu.zut.mfs.dto.JwtDto;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+
+public class JwtUtils {
+
+    Algorithm algorithm = Algorithm.HMAC256("JAC1O17W1F3QB9E8B4B1MT6QKYOQB36V");
+
+    public String generate(JwtDto jwtDto){
+        return JWT.create()
+                .withJWTId(jwtDto.getJwtId())
+                .withIssuer("mfs")
+                .withSubject(jwtDto.getSubject())
+                .withExpiresAt(jwtDto.getExpiresAt())
+                .withAudience(jwtDto.getAudience())
+                .withClaim("scope", jwtDto.getScope())
+                .sign(algorithm);
+    }
+}
