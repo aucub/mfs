89b2a407726b1648524e3f54ae9c8de0784ba8af
aucub <78630225+aucub@users.noreply.github.com>
2023-04-26 00:45:01 +0800

    refactor(code): simplify

--- src/main/java/cn/edu/zut/mfs/controller/QueryController.java
+++ src/main/java/cn/edu/zut/mfs/controller/QueryController.java
@@ -4,7 +4,10 @@ import cn.edu.zut.mfs.domain.ConsumeRecord;
 import cn.edu.zut.mfs.domain.PublishRecord;
 import cn.edu.zut.mfs.domain.PushMessage;
 import cn.edu.zut.mfs.service.InfluxDBService;
+import cn.edu.zut.mfs.service.MeiliSearchService;
 import cn.edu.zut.mfs.service.impl.InfluxDBServiceImpl;
+import com.meilisearch.sdk.exceptions.MeilisearchException;
+import com.meilisearch.sdk.model.Searchable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +40,11 @@ public class QueryController {
     @PostMapping("/queryPush")
     public Mono<List<PushMessage>> queryPush(@RequestParam String start, @RequestParam String stop) {
         return Mono.just(influxDBService.queryPush(start, stop));
+    }
+
+    @PreAuthorize("hasRole('query')")
+    @PostMapping("/search")
+    public Mono<Searchable> search(String uid, String keyword, Integer offset) throws MeilisearchException {
+        return Mono.just(MeiliSearchService.search(uid, keyword, offset));
     }
 }
--- src/main/java/cn/edu/zut/mfs/service/MeiliSearchService.java
+++ src/main/java/cn/edu/zut/mfs/service/MeiliSearchService.java
@@ -3,15 +3,25 @@ package cn.edu.zut.mfs.service;
 import com.meilisearch.sdk.Client;
 import com.meilisearch.sdk.Config;
 import com.meilisearch.sdk.Index;
+import com.meilisearch.sdk.SearchRequest;
 import com.meilisearch.sdk.exceptions.MeilisearchException;
+import com.meilisearch.sdk.model.Searchable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MeiliSearchService {
-    private static Client client = new Client(new Config("http://localhost:7700", "root"));
+    private static final Client client = new Client(new Config("http://localhost:7700", "root"));
 
     public static void store(String document, String uid) throws MeilisearchException {
         Index index = client.index(uid);
         index.addDocuments(document);
+    }
+
+    public static Searchable search(String uid, String keyword, Integer offset) throws MeilisearchException {
+        Index index = client.index(uid);
+        return index.search(
+                new SearchRequest(keyword)
+                        .setShowMatchesPosition(true).setCropLength(500).setLimit(100).setOffset(offset)
+        );
     }
 }
